dbf70aa21ea727502d1223431b86478f39064d6b
sanjay <ksanjayk@protonmail.com>
2022-05-31 13:10:33 +0530

    basic route setup

---
+++ controllers/products.js
@@ -0,0 +1,16 @@
+const getAllProductsStatic = async (req,res)=>{
+
+    res.status(200).json({msg:"product testing route"})
+    
+}
+
+const getAllProducts= async (req,res)=>{
+
+    res.status(200).json({msg:"product testing route"})
+    
+}
+
+module.exports ={
+    getAllProducts,
+    getAllProductsStatic
+}
